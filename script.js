a6af403a45ca094e35d022c0540a2bd222dbe48a
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-13 19:10:45 +0100

    Make a function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const inputWords = []
-while (true) {
-  const inputWord = prompt('Input a word (blank for finish)')
-  if (inputWord === '') {
-    break
-  }
-  inputWords.push(inputWord)
-}
-
+const inputWords = getWords()
 const wordsJoinedTogether = inputWords.join(' ')
-
 console.log(wordsJoinedTogether)
+
+function getWords() {
+  const inputWords = []
+  while (true) {
+    const inputWord = prompt('Input a word (blank for finish)')
+    if (inputWord === '') {
+      return inputWords
+    }
+    inputWords.push(inputWord)
+  }
+}
