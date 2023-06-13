2a2fe68b7e2c35520dbcd554cf58db65683715c7
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-13 19:55:42 +0100

    List of words output

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const submitButton = document.getElementById('submit-button')
 const joinButton = document.getElementById('join-button')
 const inputBox = document.getElementById('input-box')
 const outputSection = document.getElementById('output')
+const inputWordsListElement = document.getElementById('input-words-list')
 const inputWords = []
 
 submitButton.addEventListener('click', submitWord)
@@ -11,6 +12,12 @@ function submitWord() {
   const inputWord = inputBox.value
   inputWords.push(inputWord)
   inputBox.value = ''
+  inputWordsListElement.innerHTML = ''
+  for (const word of inputWords) {
+    const newListElement = document.createElement('li')
+    newListElement.innerText = word
+    inputWordsListElement.append(newListElement)
+  }
 }
 
 function joinWords() {
